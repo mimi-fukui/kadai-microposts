547490147349b37e8ded0fc69a109e09d49645d5
m.fukui <fukuinorecruit@gmail.com>
2021-02-20 18:57:16 +0000

    favorite

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+  
+  def create
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.favorite(micropost)
+    flash[:success] = '投稿をLikeしました'
+    redirect_back(fallback_location: root_path)
+  end
+
+  def destroy
+    micropost = Micropost.find(params[:micropost_id])
+    current_user.unfavorite(micropost)
+    flash[:success] = '投稿をLikeを外しました'
+    redirect_back(fallback_location: root_path)
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :likes]
   
   def index
     @users = User.order(id: :desc).page(params[:page]).per(25)
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def new
-    @user =User.new
+    @user = User.new
   end
 
   def create
@@ -38,8 +38,13 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
-end
 
+ def likes
+   @user = User.find(params[:id])
+   @likes = @user.likes.page(params[:page])
+   counts(@user)
+ end
+end
 
 private
 
